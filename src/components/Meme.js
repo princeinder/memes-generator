c274ea70d46e9ed744d4966971e060cd4c5f65d1
admin <test>
2020-07-22 19:54:57 +0530

    master

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { SketchPicker } from "react-color";
 
 const Meme = ({
   errors,
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 import Template from "../components/Template";
 import Toolbar from "../components/Toolbar";
